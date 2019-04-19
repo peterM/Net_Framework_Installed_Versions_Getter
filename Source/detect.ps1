53e787ab0924aeaa22d152033ddc6e070b8b5278
Peter M <p.malik@malikp.eu>
2019-04-19 21:23:44 +0200

    Add download link

--- Source/detect.ps1
+++ Source/detect.ps1
@@ -100,6 +100,7 @@ function Download-And-Install-Framework() {
     # net 4.5.2 -> http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe
     # net 4.6   -> http://download.microsoft.com/download/1/4/A/14A6C422-0D3C-4811-A31F-5EF91A83C368/NDP46-KB3045560-Web.exe => except win 10
     # net 4.7.2 -> https://download.microsoft.com/download/3/3/2/332D9665-37D5-467A-84E1-D07101375B8C/NDP472-KB4054531-Web.exe
+    # net 4.8   -> https://go.microsoft.com/fwlink/?linkid=2088631
     $url = "https://download.microsoft.com/download/3/3/2/332D9665-37D5-467A-84E1-D07101375B8C/NDP472-KB4054531-Web.exe"
     $output = "$PSScriptRoot\netFrameworkInstaller.exe"
     $start_time = Get-Date
